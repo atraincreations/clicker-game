f364469c396d136ddfe6988ab02f94ec4327afdb
Alex Underwood <underwoodapu@gmail.com>
2022-07-05 23:56:48 -0700

    Update 0.1.21
    Added new craft types.
    Adjusted UI.
    Added new items to store for future purchase.
    Bugged autominer, once it exceeds 200 ore it seems to begins to ignore timer and count crazily. These have all been disabled for now.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let stats = {
     ore: 0,
     ingot: 0,
     sword: 0,
+    ironRod: 3,
+    ironGear: 3,
+    orePerSwing: 1,
     automining: false,
     hasPurchasedMining: false,
     autosmelting: false,
@@ -13,6 +16,10 @@ let stats = {
 let materialsNeeded = {
     ingot: 10,
     sword: 3,
+    ironGear: 3,
+    craftIronGear: 2, //ingots required
+    ironRod: 3,
+    craftIronRod: 7, //ore required
 }
 
 let materialsValue = {
@@ -35,15 +42,24 @@ $(window).on('load', function(){
         stats.ore = myStats.ore;
         stats.ingot = myStats.ingot;
         stats.sword = myStats.sword;
-        stats.automining = stats.automining;
-        stats.hasPurchasedMining = stats.hasPurchasedMining;
+        stats.ironRod = myStats.ironRod;
+        stats.ironGear = myStats.ironGear;
+        stats.orePerSwing = myStats.orePerSwing;
+        stats.automining = myStats.automining;
+        stats.hasPurchasedMining = myStats.hasPurchasedMining;
+        updateStatsView();
+    }
+    let myMaterialsNeeded = JSON.parse(localStorage.getItem("myMaterialsNeeded"));
+    if(myMaterialsNeeded){
+        materialsNeeded.ironGear = myMaterialsNeeded.ironGear;
+        materialsNeeded.ironRod = myMaterialsNeeded.ironRod;
         updateStatsView();
     }
 })
 
 
 function autoMine(){
-    stats.ore = stats.ore + 1;
+    stats.ore = stats.ore + stats.orePerSwing;
     updateStatsView();
 }
 
@@ -52,6 +68,13 @@ function updateStatsView() {
     $('#ore-text-value').text(stats.ore);
     $('#ingot-text-value').text(stats.ingot);
     $('#sword-text-value').text(stats.sword);
+    $('#iron-ore-value-text').text("$ "+materialsValue.oreValue);
+    $('#iron-ingot-value-text').text("$ "+materialsValue.ingotValue);
+    $('#iron-sword-value-text').text("$ "+materialsValue.swordValue);
+    $('#upgrade-rod-amount-text').text(materialsNeeded.ironRod);
+    $('#rod-text-value').text(stats.ironRod);
+    $('#upgrade-gear-amount-text').text(materialsNeeded.ironGear);
+    $('#gear-text-value').text(stats.ironGear);
     if(stats.hasPurchasedMining === true){
         $('#buy-automining-btn').val('Purchased')
         $('#buy-automining-btn').prop('disabled', true);
@@ -60,6 +83,7 @@ function updateStatsView() {
         setTimeout('autoMine()', 2500);
     }
     localStorage.setItem("myStats", JSON.stringify(stats)); //store stats
+    localStorage.setItem("myMaterialsNeeded", JSON.stringify(materialsNeeded)); //store materials
 }
 
 function clearErrorText() {
@@ -71,7 +95,7 @@ function clearErrorText() {
 function miningCooldown(){
     $('#mine-btn').prop('disabled', false);
     $('#mine-btn').val('Mine');
-    stats.ore = stats.ore + 1;
+    stats.ore = stats.ore + stats.orePerSwing;
     updateStatsView();
 }
 
@@ -86,6 +110,20 @@ function swordCooldown(){
     $('#craft-sword-btn').prop('disabled', false);
     $('#craft-sword-btn').val('Craft a Sword');
     stats.sword = stats.sword + 1;
+    updateStatsView();
+}
+
+function rodCooldown(){
+    $('#smelt-rod-btn').prop('disabled', false);
+    $('#smelt-rod-btn').val('Smelt Iron Rod');
+    stats.ironRod = stats.ironRod + 1;
+    updateStatsView();
+}
+
+function gearCooldown(){
+    $('#craft-gear-btn').prop('disabled', false);
+    $('#craft-gear-btn').val('Craft Iron Gear');
+    stats.ironGear = stats.ironGear + 1;
     updateStatsView();
 }
 
@@ -135,6 +173,34 @@ $('#craft-sword-btn').on("click", function(){
     }
 })
 
+$('#smelt-rod-btn').on("click", function(){
+    clearErrorText();
+    if(stats.ore >= materialsNeeded.craftIronRod){
+        stats.ore = stats.ore - materialsNeeded.craftIronRod;
+        $('#smelt-rod-btn').val('Smelting..');
+        $('#smelt-rod-btn').prop('disabled', true);
+        updateStatsView();
+        setTimeout('rodCooldown()', 15000);
+    } else {
+        $('#error-text').text("Insufficient materials");
+    }
+})
+
+$('#craft-gear-btn').on("click", function(){
+    clearErrorText();
+    if(stats.ingot >= materialsNeeded.craftIronGear){
+        stats.ingot = stats.ingot - materialsNeeded.craftIronGear;
+        $('#craft-gear-btn').val('Crafting..');
+        $('#craft-gear-btn').prop('disabled', true);
+        updateStatsView();
+        setTimeout('gearCooldown()', 7000);
+    } else {
+        $('#error-text').text("Insufficient materials");
+    }
+})
+
+
+
 //Sell buttons
 $('#sell-ore-btn').on("click", function(){
     stats.currency = stats.currency + (stats.ore * materialsValue.oreValue);
@@ -164,4 +230,18 @@ $('#buy-automining-btn').on("click", function(){
         $('#shop-am-error-text').text(" Insufficient funds.")
     }
     
+})
+
+//bugged with automining it incriments rapidly
+$('#upgrade-ore-per-swing-btn').on("click", function(){
+    if(stats.ironGear >= materialsNeeded.ironGear && stats.ironRod >= materialsNeeded.ironRod){
+        stats.orePerSwing = stats.orePerSwing + 1;
+        stats.ironGear = stats.ironGear - materialsNeeded.ironGear;
+        stats.ironRod = stats.ironRod - materialsNeeded.ironRod;
+        materialsNeeded.ironGear = materialsNeeded.ironGear * (Math.floor(Math.random() * 5) + 2);
+        materialsNeeded.ironRod = materialsNeeded.ironRod * (Math.floor(Math.random() * 4) + 2);
+        updateStatsView();
+    } else {
+        $('#shop-upgrade-error-text').text(" Insufficient materials.")
+    }
 })
